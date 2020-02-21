910bebfb33b56bcd3b36177e2d671c20fdbe67ca
ericlugo <ericlugo@users.noreply.github.com>
2020-02-21 01:28:35 -0500

    set up Sentry Exception Monitoring
    remove logo.svg and serviceWorker.js as not needed at the moment
    modify .gitignore to ensure .env files are ignored
    install sentry
    modify index.js to:
    - initialize sentry if in production
    - notify if in mode other than production (will be removed later)
    modify App.js to clean HelloWorld page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import * as Sentry from '@sentry/browser';
+
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+import './index.css';
+
+if (process.env.REACT_APP_ENV !== 'production') {
+  console.groupCollapsed(`App launched in ${process.env.REACT_APP_ENV} mode.`);
+  console.log(`Sentry is disabled.`);
+  console.groupEnd();
+} else {
+  Sentry.init({
+    release: `${process.env.REACT_APP_NAME}@${process.env.REACT_APP_VERSION}`,
+    dsn: `${process.env.REACT_APP_SENTRY_DSN}`,
+  });
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
