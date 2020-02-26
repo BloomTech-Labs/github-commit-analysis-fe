2878c6827f077fc7ed62ca18c85a5799fe58f817
ericlugo <ericlugo@users.noreply.github.com>
2020-02-26 02:24:27 -0500

    update to make sure placeholder test passes

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 export default (props) => (
   <div>
-    <h1>The content below is inside of the Layout component!</h1>
+    <h1>Hello World</h1>
+    <p>The content below is inside of the Layout component!</p>
     {props.children}
   </div>
 );
