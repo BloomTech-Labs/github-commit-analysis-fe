ada1fab434315a29da8887d2be76c283593f562f
ittyBittyBytes <loralie.flint0128@gmail.com>
2020-06-04 12:21:22 -0700

    got the about cards back to normal and linkedin opens new tab

--- src/pages/NewDashboard.js
+++ src/pages/NewDashboard.js
@@ -3,17 +3,16 @@ import styled from "styled-components";
 import ".../../font-awesome/css/font-awesome.min.css";
 import ImageHolder from "../img/DashboardPlaceholder.jpg";
 
-
 const Holder = styled.div`
   display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  margin:  auto;
+  justify-content: center;
 `;
 
 const SideOne = styled.div`
   flex-direction: column;
-  margin: 10px 0px 30px 20px;
+  margin: 10px 0px 30px 0px;
+  border-right: 1px solid #dcdcdc;
+  // padding-right: 5px;
 `;
 
 const SideTwo = styled.div`
@@ -22,103 +21,113 @@ const SideTwo = styled.div`
 `;
 
 const RepoCard = styled.div`
-  border: 1px solid black;
+  border-bottom: 1px solid #dcdcdc;
   margin: 10px 0px 10px 0px;
   padding: 1%;
   height: auto;
 `;
 
 const ProfilePic = styled.img`
-margin: 80px 0px 10px 30px;
+  margin: 80px 0px 10px 30px;
   width: 120px;
   height: 100px;
 `;
 
 const SearchRepos = styled.div`
-display: flex;
-flex-direction: row;
-margin: 20px 0px 10px 0px;
+  display: flex;
+  flex-direction: row;
+  margin: 20px 1px 10px 0px;
 `;
 
 const SearchUsers = styled.div`
-display: flex;
-justify-content: flex-end;
-margin: 80px 0px 0px 0px;
+  display: flex;
+  justify-content: flex-end;
+  margin: 80px 40px 0px 0px;
 `;
 
 const ToggleHolder = styled.div`
-display: flex;
-flex-direction: row;
-margin: 10px 65px 10px 0px
-`
+  display: flex;
+  flex-direction: row;
+  margin: 60px 65px 10px 0px;
+`;
 
 const Toggle = styled.div`
-flex-direction: row;
-Font-weight: bold;
-color: black;
-width: 150px;
-background-color: gray;
-margin: 10px 60px 10px 100px
-`
-const BoardHolder = styled.div`
-margin: 10px 0px 300px 0px;
-display: flex;
-flex-direction: row;
-justify-content: space-evenly;
-`
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+  font-weight: bold;
+  color: #0a9aec;
+  width: 150px;
+  background-color: gray;
+  margin: 10px 60px 10px 100px;
+  padding: 10px;
+  border-radius: 10px;
+  background-color: white;
+  border: 2px solid #0a9aec;
+  &:hover {
+    background-color: #0a9aec;
+    color: white;
+  }
+`;
 
+const BoardHolder = styled.div`
+  margin: 50px 40px 100px 30px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  border: 2px solid gray;
+  max-width: 99%;
+`;
 const BoardOne = styled.div`
-border: 1px solid gray;
-flex-direction: row;
-width: 330px;
-height: 500px;
-margin: 10px 30px 30px 30px;
-padding: 5px;
-`
+  border: 1px solid gray;
+  flex-direction: row;
+  width: 450px;
+  height: 500px;
+  padding: 5px;
+`;
 
 const BoardTwo = styled.div`
-flex-direction: row;
-border: 1px solid gray;
-width: 330px;
-margin: 10px 0px 30px 30px;
-height: 500px;
-padding: 5px;
-`
+  flex-direction: row;
+  border: 1px solid gray;
+  width: 450px;
+  height: 500px;
+  padding: 5px;
+`;
 
 const MyDashboard = () => {
-
   return (
     <Holder>
       <SideOne>
         <ProfilePic src={ImageHolder} alt="profile pic"></ProfilePic>
         <SearchRepos>
           <input
+            id="input"
             type="text"
             name="search"
             placeholder="Search Repos"
           />
-          <button className="RepoButton">
+          <button id="buttons">
             <i className="fa fa-search fa-lg" aria-hidden="true"></i>
           </button>
         </SearchRepos>
         <div>
-          <RepoCard>
+          <RepoCard className="repoCard">
             <p>repo 1</p>
             <p># of commits</p>
           </RepoCard>
-          <RepoCard>
+          <RepoCard className="repoCard">
             <p>repo 1</p>
             <p># of commits</p>
           </RepoCard>
-          <RepoCard>
+          <RepoCard className="repoCard">
             <p>repo 1</p>
             <p># of commits</p>
           </RepoCard>
-          <RepoCard>
+          <RepoCard className="repoCard">
             <p>repo 1</p>
             <p># of commits</p>
           </RepoCard>
-          <RepoCard>
+          <RepoCard className="repoCard">
             <p>repo 1</p>
             <p># of commits</p>
           </RepoCard>
@@ -126,25 +135,24 @@ const MyDashboard = () => {
       </SideOne>
       <SideTwo>
         <SearchUsers>
-          <input
-            type="text"
-            name="search"
-            placeholder="Search Users">
-            </input>
-            <button className="UsersButton">
-              <i class="fa fa-search fa-lg" aria-hidden="true"></i>
-            </button>
+          <input type="type" name="search" placeholder="Search Users" />
+          <button className="UsersButton">
+            <i className="fa fa-search fa-lg" aria-hidden="true"></i>
+          </button>
         </SearchUsers>
+
         <ToggleHolder>
-            <Toggle>Slider 1</Toggle>
-            <Toggle>Slider 2</Toggle>
-            <Toggle>Slider 3</Toggle>
+          <Toggle>Slider 1</Toggle>
+          <Toggle>Slider 2</Toggle>
+          <Toggle>Slider 3</Toggle>
         </ToggleHolder>
         <BoardHolder>
           <BoardOne>
             <h6>box one</h6>
           </BoardOne>
-          <BoardTwo><h6>box two</h6></BoardTwo>
+          <BoardTwo>
+            <h6>box two</h6>
+          </BoardTwo>
         </BoardHolder>
       </SideTwo>
     </Holder>
