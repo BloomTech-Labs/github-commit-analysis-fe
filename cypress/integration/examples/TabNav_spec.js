096b48bd187bccbc7756d386cc29ef5cea1f39b8
mattorth <orth.mj@gmail.com>
2020-07-08 18:55:51 -0500

    removed tab test file

--- cypress/integration/examples/TabNav_spec.js
+++
@@ -1,7 +0,0 @@
-describe('Graphics show up on tabs', () => {
-    it('shows issues comments graph', () => {
-        cy.visit('localhost:3000')
-        cy.contains('Login with GitHub').click()
-        cy.contains('Kubernetes Issues Comments')
-    })
-})
