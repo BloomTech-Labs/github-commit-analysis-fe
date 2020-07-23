d1e20e36863f072c1a953301fbb36718ae0d0a33
ittyBittyBytes <loralie.flint0128@gmail.com>
2020-07-22 20:04:36 -0700

    trying to push from branch but master is ahead

--- src/components/ToggleStarred.js
+++ src/components/ToggleStarred.js
@@ -1,51 +1,45 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import axios from "axios";
-import { useAppState } from '../context/app-state-context';
+import { useAppState } from "../context/app-state-context";
 
 import RepoListContext from "../context/RepoListContext";
- 
+
 function ToggleStarred(repository) {
+  const [classes, setClasses] = useState(true);
   const {
-    state: { token }
+    state: { token },
   } = useAppState();
 
   const { refresh, setRefresh } = useContext(RepoListContext);
- 
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    e.stopPropagation() 
-    const update = 
-      {
-        "update": !repository.repository.isStarred,
-        "repoId": repository.repository.id
-    }
+    e.stopPropagation();
+    setClasses(!classes);
+    const update = {
+      update: !repository.repository.isStarred,
+      repoId: repository.repository.id,
+    };
     axios
       .put(`${process.env.REACT_APP_BACKEND_URL}/repo/starred`, update, {
         headers: { Authorization: `Bearer ${token}` },
       })
       .then((res) => {
         repository.repository.isStarred = !repository.repository.isStarred;
-         e.className = "notStarred";
-        setRefresh(!refresh)
+        setRefresh(!refresh);
       })
-      .catch((err) => console.log(err));      
+      .catch((err) => console.log(err));
   };
- 
+
   return (
     <div>
-      {repository.className === "notStarred" ? (
-        <i
-          onClick={handleSubmit}
-          className="fa fa-star icon-md notStarred"
-          aria-hidden="true"
-        ></i>
-      ) : (
-        <i
-          onClick={handleSubmit}
-          className="fa fa-star icon-md star"
-          aria-hidden="true"
-        ></i>
-      )}
+      <i
+        onClick={handleSubmit}
+        className={
+          classes ? "fa fa-star icon-xl star" : "fa fa-star icon-xl notStarred"
+        }
+        aria-hidden="true"
+      ></i>
     </div>
   );
 }
